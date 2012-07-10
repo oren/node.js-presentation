637750a9a498af895713ce86af70cb7ed3afdfb0
Oren <ogolan@attinteractive.com>
2012-07-10 06:53:30 -0700

    comments about ChildProcess object

--- code/streams/server_resize.js
+++ code/streams/server_resize.js
@@ -3,18 +3,19 @@ var http = require('http');
 var fs = require('fs');
 var spawn = require('child_process').spawn;
  
-// request is a readable stream (event emitter with data, end and close events)
-// response is a writable stream (event emitter with the close event)
+// /1.jpg/400x400
 http.createServer(function (request, response) {
   parts = request.url.split('/');
   console.log(parts);
 
   var path = __dirname + '/guinness/' + parts[1];
   var args = [path, '-resize', parts[2], '-'];
-  var convert = spawn('convert', args);
+  var convert = spawn('convert', args); //convert is a ChildProcess object.
+                                        // it's an event emitter that emit 3 events:
+                                        // stdin, out and err
   convert.stdout.pipe(response);
   convert.stderr.pipe(process.stderr);
 
-}).listen(3000);
+}).listen(3001);
  
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:3001/');
