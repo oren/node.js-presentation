944072be760b2f43ca6fd37d93a9936eabd85855
Oren <ogolan@attinteractive.com>
2012-07-10 06:31:28 -0700

    streaming from remote server

--- code/streams/server.js
+++ code/streams/server.js
@@ -1,17 +1,65 @@
-// stream an image from the disk
+// stream an image from the disk or from a remote server (EC2, etc) 
+
 var http = require('http');
 var fs = require('fs');
+var requestImage = require('request');
  
 // request is a readable stream (event emitter with data, end and close events)
 // response is a writable stream (event emitter with the close event)
 http.createServer(function (request, response) {
   var path = __dirname + '/guinness' + request.url
-  console.log(path);
+  var stream = fs.createReadStream(path); // stream is a readable stream
 
-  var stream = fs.createReadStream(path); // return a readable stream
   stream.pipe(response);
-
   stream.on('error', console.error);
-}).listen(3000);
+}).listen(3001);
  
-console.log('Server running at http://localhost:3000/');
+console.log('Server running at http://localhost:3001/');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// requestImage('http://mcgonnigle.files.wordpress.com/2007/07/coonbig.jpg').pipe(response);
+// var stream = requestImage('http://mcgonnigle.files.wordpress.com/2007/07/coonbig.jpg');
+
+// stream.on('data', function(chunk){
+//   console.log('chunk arrived!', chunk);
+//   response.write(chunk);
+// });
