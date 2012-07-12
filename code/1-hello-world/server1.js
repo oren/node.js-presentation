97cccb7f843f13f0810b0b607de3cf432b894082
Oren <ogolan@attinteractive.com>
2012-07-12 09:49:31 -0700

    comments

--- code/1-hello-world/server1.js
+++ code/1-hello-world/server1.js
@@ -12,4 +12,5 @@ console.log('Server running at http://localhost:3001/');
 // the request object is created internally by HTTP server. it's a Readable Stream and en event emitter. it emits 'data', 'end' and others.
 // it alsa has some functions. one of them is 'pipe' and allow us to push data into a Writable Stream.
 
-// the respose object is Writable Stream. 
+// the respose object is Writable Stream. 2 if it's functions are 'end' and 'write'
+// write will write a string with a given encoding  to the stream. end terminates the stream with EOF of FIN.
