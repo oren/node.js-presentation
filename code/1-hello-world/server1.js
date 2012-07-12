9d340aceb5902f9c320bd57e76723c4a99b367be
Oren <ogolan@attinteractive.com>
2012-07-12 09:39:35 -0700

    comments

--- code/1-hello-world/server1.js
+++ code/1-hello-world/server1.js
@@ -5,3 +5,11 @@ http.createServer(function (request, response) {
 }).listen(3001);
  
 console.log('Server running at http://localhost:3001/');
+
+
+// http.createServer return http.Server object which is an event emitter. one of it's events is 'request'
+
+// the request object is created internally by HTTP server. it's a Readable Stream and en event emitter. it emits 'data', 'end' and others.
+// it alsa has some functions. one of them is 'pipe' and allow us to push data into a Writable Stream.
+
+// the respose object is Writable Stream. 
--- code/1-hello-world/server2.js
+++ code/1-hello-world/server2.js
@@ -8,3 +8,9 @@ server.on('request', function (request, response) {
   response.end('Hello World\n');
 });
  
+// http.createServer return http.Server object which is an event emitter. one of it's events is 'request'
+
+// the request object is created internally by HTTP server. it's a Readable Stream and en event emitter. it emits 'data', 'end' and others.
+// it alsa has some functions. one of them is 'pipe' and allow us to push data into a Writable Stream.
+
+// the respose object is Writable Stream. 
