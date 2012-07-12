b013b1bd58be11f9a3e1ac4aef20cd9ddf8b9250
Oren <ogolan@attinteractive.com>
2012-07-12 07:24:11 -0700

    debug

--- code/1-hello-world/server3.js
+++ code/1-hello-world/server3.js
@@ -1,11 +1,15 @@
 // the request is not blocked!
 // our server can accept more requests
+// demo the node debugger
 
 var http = require('http');
+var users = 0;
  
 http.createServer(function (request, response) {
+  users += 1;
   response.write('Hello\n');
   setTimeout(function(){
+    debugger;
     response.end('world\n');
   }, 5000);
 }).listen(3001);
